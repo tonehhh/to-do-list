31194b083249e3d0ac7f853aea0c4627624c6ca3
tonehhh <tonywhitmarsh93@gmail.com>
2023-02-10 20:56:11 +0000

    added functions etc.

---
+++ script.js
@@ -0,0 +1,50 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+
+function getItems(){
+    const value = localStorage.getItem("todo-list") || "[]";
+
+    return JSON.parse(value);
+}
+
+function setItems(items){
+    const itemsJson = JSON.stringify(items);
+
+    localStorage.setItem("todo-list", itemsJson)
+}
+
+function addItem(){
+    items.unshift({
+        description: " ",
+        completed: false
+    });
+
+    setItems(items);
+    refreshList();
+}
+
+function refreshList(){
+    // TODOLIST: sort lists
+
+    ITEMS_CONTAINER.innerHTML = "";
+
+    for (const item of items){
+        const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+        const descriptionInput = itemElement.querySelector(".itemdescription");
+        const completedInput = itemElement.querySelector(".itemcompleted");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        ITEMS_CONTAINER.append(itemElement);
+    }
+}
+
+ADD_BUTTON.addEventListener("click", () => {
+    addItem();
+});
+
+refreshList();
